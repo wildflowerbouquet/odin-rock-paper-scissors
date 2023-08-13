039e007a249728d6c6f63497fb5c231ff390240d
wildflower <wildflower@rt.mozmail.com>
2023-08-13 12:35:36 -0400

    Pseudocode draft 1.

---
+++ rockpaperscissors.js
@@ -0,0 +1,12 @@
+/* PSUEDOCODE
+
+When a user inputs "Rock", "Paper", or "Scissors"
+The computer randomly returns "Rock", "Paper", or "Scissors"
+Print the user's input and the computer's return.
+If the user's input beats the computer's return, print "You win! + results".
+If the computer's return beats the user's in put, the comptuer "You lose! + results".
+Keep track of winner and lose for five rounds.
+Print overall winner.
+
+
+*/
