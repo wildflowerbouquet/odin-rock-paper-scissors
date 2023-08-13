7709284b229e44a9d28acaa673528283083b736c
wildflower <wildflower@rt.mozmail.com>
2023-08-13 15:57:47 -0400

    Created function playRound.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -17,15 +17,40 @@ function getComputerChoice() {
     const min = 1;
     const max = 3;
 
-    const choice = (Math.floor(Math.random()*(max-min+1)+min));
+    const x = (Math.floor(Math.random()*(max-min+1)+min));
 
-    if (choice === 1) {
-        return "Rock";
-    } else if (choice === 2) {
-        return "Paper";
-    } else if (choice === 3) {
-        return "Scissors";
+    if (x === 1) {
+        return "rock";
+    } else if (x === 2) {
+        return "paper";
+    } else if (x === 3) {
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
+        return "You both chose Rock. It's a tie!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper") {
+        return "You both chose Paper. It's a tie!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors") {
+        return "You both chose Scissors. It's a tie!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors!";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper!";
+    }
+}
+
+const playerSelection = prompt("Rock, Paper, or Scissors?");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
